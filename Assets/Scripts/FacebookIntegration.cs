fbb0e712ff7d00a84d9a60f00a613917b0f60bbc
Testing <kulwantsingh@Kulwants-MacBook-Air.local>
2018-10-10 17:23:39 +0530

    New Features
    Added play button
    Made the game more responsive.
    Facebook integration under process

---
+++ Assets/Scripts/FacebookIntegration.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Facebook.Unity;
+using System.Reflection;
+
+public class FacebookIntegration : MonoBehaviour {
+
+	public Text username;
+	public Image userprof;
+
+	void Awake(){
+	
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -19,7 +19,7 @@ public class GameControl : MonoBehaviour {
 	private int score=0;
 	//Score Text Variable
 	public Text scoreText;
-
+	//Highscore tet reference
 	public Text highScore;
 
 	void Awake(){
@@ -33,11 +33,12 @@ public class GameControl : MonoBehaviour {
 	void Update () {
 		//Restart the Game by tapping
 		if (gameOver == true && Input.GetMouseButtonDown (0)) {
-			//
+			//Restart the scene whichis currently loaded
 			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
 		}
 	}
 	void Start(){
+		//Showing the stored highscore 
 		highScore.text = PlayerPrefs.GetInt ("highScore").ToString();
 	}
 	//method to check if the bird  scored or not 
@@ -46,8 +47,11 @@ public class GameControl : MonoBehaviour {
 			return;
 		}
 		score++;
+		//Getting current user score
 		scoreText.text = "Score:" + score.ToString ();
+		//storing it in the player prefs
 		PlayerPrefs.SetInt ("score",score);
+		//checking for the valid highscore
 		if (PlayerPrefs.HasKey ("highScore")) {
 			if (score>PlayerPrefs.GetInt("highScore")) {
 				PlayerPrefs.SetInt ("highScore",score);
